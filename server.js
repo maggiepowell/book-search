e8081f5ccd9bbef71c3eb30ebdd3fe90babc0426
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-04-26 18:26:34 -0700

    added googlebooks db and Book collection

--- server.js
+++ server.js
@@ -3,6 +3,25 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const mongoose = require('mongoose');
+
+
+// this is our MongoDB database
+const dbRoute = "mongodb://googlebooks";
+
+// connects our back end code with the database
+mongoose.connect(
+  dbRoute,
+  { useNewUrlParser: true }
+);
+
+let db = mongoose.connection;
+
+db.once("open", () => console.log("connected to the database"));
+
+// checks if connection with the database is successful
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
