a09f0dda7f1ce07a3a27736b6f381bef438362c3
Maggie Powell <maggiepowell@Maggies-MacBook-Pro.local>
2019-04-30 15:11:00 -0700

    working on css

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -3,24 +3,25 @@ import { Link } from "react-router-dom";
 
 function NavTabs() {
     return (
-        <ul className="nav nav-tabs">
-        <li className="nav-item">
+        <div className="nav nav-tabs">
+                Google Book Search
+        <div className="nav-item">
           <Link
             to="/Saved"
             className={window.location.pathname === "/Saved" ? "nav-link active" : "nav-link"}
           >
             Saved
           </Link>
-        </li>
-        <li className="nav-item">
+        </div>
+        <div className="nav-item">
           <Link
             to="/Search"
             className={window.location.pathname === "/Search" ? "nav-link active" : "nav-link"}
           >
             Search
           </Link>
-        </li>
-      </ul>
+        </div>
+      </div>
     );
   }
   
